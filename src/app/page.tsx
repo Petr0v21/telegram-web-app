18c868d265a5c8ad152cad55e0473ad3ed880dac
Petr0v21 <petr0v21vs@gmail.com>
2024-05-29 23:15:06 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import { useTelegram } from "./telegram.provider";
 
 export default function Home() {
+  const [counter, setCounter] = useState<number>(0);
+  const telegram = useTelegram();
+
+  const handleMainButtonClick = useCallback(() => {
+    telegram.showAlert(`You clicked ${counter} times!`);
+  }, [counter, telegram]);
+
+  useEffect(() => {
+    telegram.MainButton.setParams({
+      text: "CLICK ON ME",
+      is_active: true,
+      is_visible: true,
+    });
+  }, [telegram]);
+
+  useEffect(() => {
+    telegram.onEvent("mainButtonClicked", handleMainButtonClick);
+    return () => telegram.offEvent("mainButtonClicked", handleMainButtonClick);
+  }, [handleMainButtonClick, telegram]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <>
+      <h2>Hello, {telegram.initDataUnsafe?.user?.first_name || "user"}</h2>
+      <p>Let&apos;s create a Telegram Web App!</p>
+      <div>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <span>Counter</span>
+          {counter}
         </div>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
