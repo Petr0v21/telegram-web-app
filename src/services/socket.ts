193309aefa5cf841c80172a4ec55b3e58a421e80
Petr0v21 <petr0v21vs@gmail.com>
2024-06-03 23:55:07 +0300

    test1

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 export const socket = io(process.env.NEXT_PUBLIC_BACK_SOCKET_URL!, {
   transports: ["websocket"],
   extraHeaders: {
-    "key-token": "test" + (window as any).Telegram.WebApp.initDataUnsafe,
+    "key-token": "test",
   },
 }).connect();
 
