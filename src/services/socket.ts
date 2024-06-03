af06b735571671c2ab95c034dbbcbfe9e5822867
Petr0v21 <petr0v21vs@gmail.com>
2024-06-03 23:50:35 +0300

    socket test

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,7 +2,12 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_BACK_SOCKET_URL!, {transports: ['websocket']}).connect();
+export const socket = io(process.env.NEXT_PUBLIC_BACK_SOCKET_URL!, {
+  transports: ["websocket"],
+  extraHeaders: {
+    "key-token": "test" + (window as any).Telegram.WebApp.initDataUnsafe,
+  },
+}).connect();
 
 export enum EventType {
   UpdateUserBalance = "UserBalance",
